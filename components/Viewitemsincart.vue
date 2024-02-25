b05f64c92414061796b1cc907110979ecbbb2fd2
MACK <66022477@up.ac.th>
2024-02-26 00:53:23 +0700

    mmmmmm

---
+++ components/Viewitemsincart.vue
@@ -0,0 +1,20 @@
+<template>
+    <a href="mycart">
+        <div class="addbutton3">
+            ดูสินค้าในตะกร้า
+        </div>
+    </a>
+</template>
+<style>
+.addbutton3{
+    height: 30px;
+    width: 150px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    color: #58790C;
+    font-size: 16px;
+    border-radius: 5px;
+    
+}
+</style>
