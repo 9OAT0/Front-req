22b79538b35f0e6f0bc22b287f8d6f87a12c8695
MACK <66022477@up.ac.th>
2024-02-27 17:26:46 +0700

    bnbbbb

--- components/product2.vue
+++ components/product2.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="w-[280px] h-[400px] mr-[8px] ">
+    <div class="product-items">
         <div class="ช่องใส่รูป">
             <div class="รูป22">
                 <img src="/Blend.jpg">
@@ -12,17 +12,23 @@
             ฿145
         </div>
         <div class="flex justify-center">
-        <a href="Productdetail">
-            <div class="bg-CartBG w-[160px] h-[50px] flex items-center justify-center rounded-lg text-[20px] text-white piot cursor-pointer">
-                <i class="fa-solid fa-cart-shopping mr-4"></i>
-            สั่งซื้อสินค้า
-            </div>
-        </a>
+            <a href="Productdetail">
+                <div class="bg-CartBG w-[160px] h-[45px] flex items-center justify-center rounded-lg text-[20px] text-white piot cursor-pointer">
+                    <i class="fa-solid fa-cart-shopping mr-4"></i>
+                สั่งซื้อสินค้า
+                </div>
+            </a>
         </div>
     </div>
-
 </template>
 <style>
+.product-items {
+    width: 280px;
+    height: 400px;
+    margin-right: 8px;
+    margin-bottom: 10px;
+    border-radius: 5px;
+}
 .ช่องชื่อสินค้า {
     height: 44px;
     width: 280px;
